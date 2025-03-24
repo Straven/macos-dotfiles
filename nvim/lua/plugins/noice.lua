e1675f987d54cfc91db5745d7d1e47102f4f7f50
Mykola Sarry <sarry.nikolaj@gmail.com>
2025-03-24 06:59:41 +0200

    added noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,66 +1,47 @@
--- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/noice.lua
--- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/noice.lua
-
--- I want to change the default notifications to be less obtrussive (if that's even a word)
--- https://github.com/folke/noice.nvim
-
 return {
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      presets = {
-        -- This is the search bar or popup that shows up when you press /
-        -- Setting this to false makes it a popup and true the search bar at the bottom
-        -- search middle
-        bottom_search = false,
-      },
-      messages = {
-        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-        -- This is a current Neovim limitation.
-        enabled = true, -- enables the Noice messages UI
-        view = "mini", -- default view for messages
-        view_error = "mini", -- view for errors
-        view_warn = "mini", -- view for warnings
-        view_history = "mini", -- view for :messages
-        view_search = "mini", -- view for search count messages. Set to `false` to disable
-      },
-      notify = {
-        -- Noice can be used as `vim.notify` so you can route any notification like other messages
-        -- Notification messages have their level and other properties set.
-        -- event is always "notify" and kind can be any log level as a string
-        -- The default routes will forward notifications to nvim-notify
-        -- Benefit of using Noice for this is the routing and consistent history view
-        enabled = true,
-        view = "mini",
-      },
-      lsp = {
-        message = {
-          -- Messages shown by lsp servers
-          enabled = true,
-          view = "mini",
-        },
-      },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+    require("noice").setup {
       views = {
-        -- This sets the position for the search popup that shows up with / or with :
         cmdline_popup = {
           position = {
-            row = "40%",
+            row = 5,
             col = "50%",
           },
+          size = {
+            width = 60,
+            height = "auto",
+          },
         },
-        mini = {
-          -- timeout = 5000, -- timeout in milliseconds
-          timeout = vim.g.neovim_mode == "skitty" and 2000 or 5000,
-          align = "center",
+        popupmenu = {
+          relative = "editor",
           position = {
-            -- Centers messages top to bottom
-            row = "95%",
-            -- Aligns messages to the far right
-            col = "100%",
+            row = 8,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
           },
         },
       },
     },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
   },
 }
